b192549affe8ecfd0743325bc100840e508c0d9d
pnski <s1l3nce@hotmail.de>
2024-03-09 10:40:55 +0100

    u_

---
+++ data/scripts/fertilities.lua
@@ -0,0 +1,6 @@
+ts.Unlock.SetUnlockNet(120244) -- unlock research feature
+for i = 1,1337 do
+    for _, j in pairs({124842,124841,124839,124840}) do
+        ts.Research.SetAddGodlikeCheat(j)
+    end
+end
